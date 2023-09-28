cecfb80d0c8725b00ee3e0fc886a0a43dcf3e315
momid <mohamad.daliran@gmail.com>
2023-09-28 15:46:40 +0330

    git ignore changes

--- src/main/kotlin/com/momid/parser/StructureFinderUsage.kt
+++ src/main/kotlin/com/momid/parser/StructureFinderUsage.kt
@@ -11,32 +11,17 @@ class While(var expression: Exp? = null, var codeBlock: CodeBlock? = null): Stru
     }
 )
 
-class Exp(): Structure(
+class Exp : Structure(
     insideParentheses(some(condition { it != '3' }))
 )
 
-class CodeBlock(): Structure(
+class CodeBlock : Structure(
     some(condition { it != '{' && it != '}' })
 )
 
-val whileText = ("while (some()true) { whi@\n" +
-        "            for (registeredClass in registeredClasses) \n" +
-        "                val structure = evaluateStructure(registeredClass, currentTokenIndex, tokens)\n" +
-        "                val structureRange = structure.range\n" +
-        "                if (structureRange != null) \n" +
-        "                    if (currentTokenIndex > tokens.lastIndex) \n" +
-        "                        continue\n" +
-        "                     else \n" +
-        "                        val nextTokenIndex = structureRange.last\n" +
-        "                        foundStructures.add(structure)\n" +
-        "                        currentTokenIndex = nextTokenIndex\n" +
-        "                        continue@whi\n" +
-        "                    \n" +
-        "                \n" +
-        "            \n" +
-        "\n" +
-        "            break\n" +
-        "        }").toList()
+val whileText = ("while (some() && true) {" +
+        "weAreInsidewhile()" +
+        "}").toList()
 
 fun main() {
 
