7c9731bed86522f33408b59ae1a5136381cb996f
momid <mohamad.daliran@gmail.com>
2023-10-09 14:24:11 +0330

    added result to continue with

--- src/main/kotlin/com/momid/parser/expression/Result.kt
+++ src/main/kotlin/com/momid/parser/expression/Result.kt
@@ -2,6 +2,6 @@ package com.momid.parser.expression
 
 open class Result<T>
 
-class Ok<T>(ok: T): Result<T>()
+class Ok<T>(val ok: T): Result<T>()
 
-class Error<T>(error: String, range: IntRange): Result<T>()
+class Error<T>(val error: String, val range: IntRange): Result<T>()
